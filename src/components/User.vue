d1d0a0398e6683f1f223793e4a7a7a578a80e8d2
Stella-cmy <2680462740@qq.com>
2020-11-17 12:32:11 +0800

    “cmy-20.11.17-update&delUser”

--- src/components/User.vue
+++ src/components/User.vue
@@ -62,10 +62,10 @@
     width="40%">
     <span>
       <el-form ref="addForm" :model="addForm" label-width="80px">
-            <el-input clas="myinput" v-model="addForm.username" placeholder="请输入用户名"></el-input> 
-            <el-input clas="myinput" v-model="addForm.password" placeholder="请输入密码"  show-password></el-input>                
-            <el-input clas="myinput" v-model="addForm.realname" placeholder="请输入姓名"></el-input>                
-            <el-select clas="myinput" v-model="addForm.usetype" placeholder="请选择用户类别">
+            <el-input class="myinput" v-model="addForm.username" placeholder="请输入用户名"></el-input> 
+            <el-input class="myinput" v-model="addForm.password" placeholder="请输入密码"  show-password></el-input>                
+            <el-input class="myinput" v-model="addForm.realname" placeholder="请输入姓名"></el-input>                
+            <el-select class="myinput" v-model="addForm.usetype" placeholder="请选择用户类别">
               <el-option
                 v-for="item in usetypes"
                 :key="item.value"
@@ -73,7 +73,7 @@
                 :value="item.value">
               </el-option>
             </el-select>            
-            <el-select clas="myinput" v-model="addForm.doctitleid" placeholder="请选择医生职称">
+            <el-select class="myinput" v-model="addForm.doctitleid" placeholder="请选择医生职称">
               <el-option
                 v-for="item in doctitleids"
                 :key="item.value"
@@ -81,7 +81,7 @@
                 :value="item.value">
               </el-option>
             </el-select>
-            <el-select clas="myinput" v-model="addForm.deptid" placeholder="请选择科室">
+            <el-select class="myinput" v-model="addForm.deptid" placeholder="请选择科室">
               <el-option
                 v-for="item in deptids"
                 :key="item.value"
@@ -89,7 +89,7 @@
                 :value="item.value">
               </el-option>
             </el-select> 
-            <el-select clas="myinput" v-model="addForm.registleid" placeholder="请选择挂号级别">
+            <el-select class="myinput" v-model="addForm.registleid" placeholder="请选择挂号级别">
             <el-option
               v-for="item in registleIDs"
               :key="item.value"
@@ -102,9 +102,60 @@
       
       </el-form>
     </span>
-    <span slot="footer" class="dialog-footer">
+    <span slot="footer" classs="dialog-footer">
       <el-button @click="dialogVisible = false">取 消</el-button>
       <el-button type="primary"  @click="addUserInfo">确 定</el-button>
+    </span>
+  </el-dialog>
+  <el-dialog
+    title="修改用户"
+    :visible.sync="dialogVisible2"
+    width="40%">
+    <span>
+      <el-form ref="editForm" :model="editForm" label-width="80px">
+        <el-input type="hidden" v-model="editForm.id"></el-input>
+            <el-input class="myinput" v-model="editForm.username" readonly></el-input> 
+            <el-input class="myinput" v-model="editForm.realname" placeholder="请输入姓名"></el-input>                
+            <el-select class="myinput" v-model="editForm.usetype" placeholder="请选择用户类别">
+              <el-option
+                v-for="item in usetypes"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>            
+            <el-select class="myinput" v-model="editForm.doctitleid" placeholder="请选择医生职称">
+              <el-option
+                v-for="item in doctitleids"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+            <el-select class="myinput" v-model="editForm.deptid" placeholder="请选择科室">
+              <el-option
+                v-for="item in deptids"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select> 
+            <el-select class="myinput" v-model="editForm.registleid" placeholder="请选择挂号级别">
+            <el-option
+              v-for="item in registleIDs"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>  
+            <el-radio v-model="editForm.isscheduling" label="是">参与排班</el-radio>
+            <el-radio v-model="editForm.isscheduling" label="否">不参与排班</el-radio>
+      
+      </el-form>
+    </span>
+    <span slot="footer" classs="dialog-footer">
+      <el-button @click="dialogVisible2 = false">取 消</el-button>
+      <el-button type="primary"  @click="updateUserInfo">确 定</el-button>
     </span>
   </el-dialog>
 
@@ -121,7 +172,67 @@ export default {
        //按条件查询关键字
         keywords:'',
         dialogVisible:false,
+        dialogVisible2:false,
         addForm:{
+          username:'',
+          password:'',
+          realname:'',
+          usetype:'',
+          doctitleid:'',
+          isscheduling:'是',
+          deptid:'',
+          registleid:'',
+        },//用户类别选项
+        usetypes: [{
+          value: '0',
+          label: '超级管理员'
+        }, {
+          value: '1',
+          label: '医院管理员'
+        }, {
+          value: '2',
+          label: '挂号收费员'
+        }, {
+          value: '3',
+          label: '门诊医生'
+        }, {
+          value: '4',
+          label: '医技医生'
+        }],
+        //医生职称选项
+         doctitleids: [{
+          value: '81',
+          label: '主任医师'
+        }, {
+          value: '82',
+          label: '副主任医师'
+        }, {
+          value: '83',
+          label: '主治医师'
+        }, {
+          value: '84',
+          label: '住院医师'
+        }],
+        //科室选择
+        deptids: [{
+          value: '1',
+          label: '心血管内科'
+        }, {
+          value: '2',
+          label: '神经内科'
+        }, {
+          value: '3',
+          label: '普通内科'
+        }],//挂号级别选择
+         registleIDs: [{
+          value: '1',
+          label: '专家号'
+        }, {
+          value: '2',
+          label: '普通号'
+        }],
+        editForm:{
+          id:'',
           username:'',
           password:'',
           realname:'',
@@ -187,9 +298,60 @@ export default {
       },
      handleEdit(index, row) {
         console.log(index, row);
+        this.dialogVisible2 = true;
+        this.editForm = row;
+        // for(var i=0;i<this.usetypes.length;i++){
+        //   if(row.usetype+''==this.usetypes[i].value){
+        //     this.editForm.usetype = this.usetypes[i].label;
+        //   }
+        // }
+        let obj = {};
+        //用户类型
+        obj = this.usetypes.find((item)=>{
+          return item.value === row.usetype+'';
+        });
+        this.editForm.usetype = obj.value;
+        //医生
+        obj = this.doctitleids.find((item)=>{
+          return item.value === row.doctitleid+'';
+        });
+        this.editForm.doctitleid = obj.value;
+        //科室
+        obj = this.deptids.find((item)=>{
+          return item.value === row.deptid+'';
+        });
+        this.editForm.deptid = obj.value;
+        //挂号
+        obj = this.registleIDs.find((item)=>{
+          return item.value === row.registleid+'';
+        });
+        this.editForm.registleid = obj.value;  
       },
      handleDelete(index, row) {
-        console.log(index, row);
+        //console.log(index, row);
+        var this_ = this;
+        this_.$confirm('此操作将永久删除该用户，是否继续？','提示',{
+          confirmButtonText:'确定',
+          cancelButtonText:'取消',
+          type:'warning'
+        }).then(()=>{
+            this.axios({
+            method:'get',
+            url:'/deleteUserById?id='+row.id
+          }).then(function(){
+            this_.$message({
+              type:'sucess',
+              message:'删除成功'
+            })
+            this_.getUsers('')
+          });
+        }).catch(()=>{
+           this_.$message({
+              type:'info',
+              message:'已取消删除'
+            })
+        })
+        
       },
       handleCurrentChange(val) {
         this.currentRow = val;
@@ -245,16 +407,55 @@ export default {
       })
       }
     },
+    //修改用户信息
+    updateUserInfo:function(){
+      var this_ = this;
+      this.axios({
+        method:'post',
+        url:'/updateUser',
+        data:this.editForm
+      }).then(function(){
+         alert("用户修改成功");
+        this_.dialogVisible2=false
+        this_.getUsers('')
+      });
+    },
+    editUserInfo:function(){
+      var this_=this;
+      //判空
+      if(this.addForm.username!=null&&this.addForm.username!=''&&
+      this.addForm.password!=null&&this.addForm.password!=''&&
+      this.addForm.realname!=null&&this.addForm.realname!=''&&
+      this.addForm.usetype!=null&&this.addForm.usetype!=''&&
+      this.addForm.deptid!=null&&this.addForm.deptid!=''){
+      this.axios({
+       method:'post',
+       url:'/addUser',
+       data:this.addForm
+      }).then(function(){
+        //成功
+        alert("用户添加成功");
+        this_.dialogVisible=false
+        this_.getUsers('')
+      })
+      }
+    },
+
   },
       mounted:function(){
       this.getUsers('')
     }
-}
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 body,html{
   height:100vh;
+}
+.myinput{
+    margin:15px;/*元素边距*/
+    width:300px;
+    opacity:1;
 }
 </style>
